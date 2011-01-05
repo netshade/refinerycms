423f3b5a88243353c0af0d429b6722885801cb37
Chris <christopher@fastestforward.com>
2011-01-05 18:24:39 -0500

    Revert "Load wymeditor using javascript based HTML and not using a remote controller action which saves several HTTP requests when loading the editor and some server overhead."
    This reverts commit 0b05f29c3806247bec2a22dec5269f7db581780e.

--- core/app/controllers/refinery/fast_controller.rb
+++ core/app/controllers/refinery/fast_controller.rb
@@ -1,4 +1,7 @@
-module Refinery
-  class FastController < ActionController::Base
+class Refinery::FastController < ActionController::Base
+
+  def wymiframe
+    render :template => "/wymiframe", :layout => false
   end
+
 end
